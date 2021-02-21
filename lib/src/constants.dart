9ba8e69b1c8e2eb2efe6f8c6fca79a00a4d6156a
rmarticedeno <rmarticedeno@gmail.com>
2021-02-21 12:16:26 -0500

    Add Test mode constant

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -4,6 +4,7 @@ class Constants {
   static const appName = "Gestion UH";
   static const primaryColor = const Color.fromARGB(255, 91, 1, 1);
   static const secondaryColor = const Color.fromARGB(160, 91, 1, 1);
+  static const TestMode = false;
 }
 
 class SampleData {
