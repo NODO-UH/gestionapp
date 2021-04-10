7e71ff9a885589afd88bf37c88b42075a031e8e9
rmarticedeno <rmarticedeno@gmail.com>
2021-04-09 22:52:44 -0400

    Add mail quota url

--- lib/src/utils/constants/misc.dart
+++ lib/src/utils/constants/misc.dart
@@ -1,8 +1,9 @@
 class Constants {
   static const appName = "Gestion UH";
-  static const TestMode = true;
+  static const TestMode = false;
   static const authUrl = "/auth/login";
   static const quotaUrl = "/proxy/quota";
+  static const mailQuotaUrl = "/email/quota";
   static const resetPasswordUrld = "/user/changepassword";
   static const baseUrl = "https://identity.sic.uh.cu";
   static const copyRight = " GestiónUH version 1.0 Pre-Alpha";
