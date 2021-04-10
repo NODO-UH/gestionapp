69b4cbc73449067a3522900d437bb2001a6b964a
rmarticedeno <rmarticedeno@gmail.com>
2021-04-10 11:23:15 -0400

    Add getAllSecurityQuestions method

--- lib/src/utils/constants/misc.dart
+++ lib/src/utils/constants/misc.dart
@@ -6,6 +6,7 @@ class Constants {
   static const mailQuotaUrl = "/email/quota";
   static const resetPasswordUrld = "/user/changepassword";
   static const userDataUrl = "/user/me";
+  static const allSecurityQuestionsUrl = "/securityquestions";
   static const baseUrl = "https://identity.sic.uh.cu";
   static const copyRight = " GestiónUH version 1.0 Pre-Alpha";
 }
