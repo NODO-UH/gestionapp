9d0a7f38f496cd7f2ffe379862968ddaa1f33a0c
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-04-11 04:31:36 -0400

    Validation and submit data for registration

--- lib/src/presentation/pages/register_page.dart
+++ lib/src/presentation/pages/register_page.dart
@@ -1,10 +1,11 @@
 import 'package:flash/flash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/models/signUpData.dart';
 import '../../utils/constants.dart';
 import '../../utils/pair.dart';
 import '../../utils/validators.dart';
-
 import '../blocs.dart';
 import '../widgets.dart';
 import '../widgets/bottom_sheet.dart';
@@ -24,6 +25,15 @@ class _RegisterPageState extends State<RegisterPage> {
   List<TextEditingController> answersTextControllers;
   TextEditingController ciController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    answersTextControllers.forEach((element) => element.dispose());
+    ciController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -36,9 +46,23 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  bool isReadyForRegistering() {
-    return questionsTaken.where((element) => element != -1).length ==
-        NUMBER_OF_SECURITY_QUESTIONS_NEEDED;
+  _onRegisterAction() {
+    if (!(_formKey.currentState?.validate() ?? false)) return false;
+    if (answersTextControllers.any((e) => e.text == '')) return false;
+    if (questionsTaken.where((element) => element != -1).length !=
+        NUMBER_OF_SECURITY_QUESTIONS_NEEDED) return false;
+    var _questions =
+        questions.map((e) => questionsTaken[e.second] >= 0 ? e : null).toList();
+    _questions.removeWhere((element) => element == null);
+    _questions.sort((e1, e2) =>
+        questionsTaken[e1.second].compareTo(questionsTaken[e2.second]));
+    return context.read<RegisterBloc>().add(FormsEnteredRegister(
+            data: SignUpData(
+          ci: ciController.text,
+          password: passwordController.text,
+          questions: _questions.map((e) => e.first).toList(),
+          answers: answersTextControllers.map((e) => e.text).toList(),
+        )));
   }
 
   @override
@@ -61,7 +85,7 @@ class _RegisterPageState extends State<RegisterPage> {
         listener: (context, state) async {
           if (state is RegisterUserFailure && state.error != null) {
             _showCenterFlash(
-              error: state.error,
+              message: state.error,
               position: FlashPosition.top,
               style: FlashStyle.floating,
             );
@@ -72,6 +96,18 @@ class _RegisterPageState extends State<RegisterPage> {
                   .read<RegisterBloc>()
                   .add(QuestionsRequestedRegister()),
             );
+          } else if (state is RegisterUserSuccess) {
+            _showCenterFlash(
+              message:
+                  'Se ha registrado correctamente, su correo electrónico es ${state.userEmail}.',
+              position: FlashPosition.top,
+              style: FlashStyle.floating,
+            );
+            Future.delayed(
+                Duration(seconds: 4),
+                () => Navigator.of(context)
+                  ..popUntil((_) => Navigator.of(context).canPop())
+                  ..pushNamed(LOGIN_ROUTE_NAME));
           }
         },
         builder: (context, state) {
@@ -85,88 +121,94 @@ class _RegisterPageState extends State<RegisterPage> {
           return SingleChildScrollView(
             child: Padding(
               padding: EdgeInsets.only(top: 30, bottom: 9, left: 18, right: 18),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    child: Text(
-                      'Todos los campos son obligatorios.*',
+              child: Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.disabled,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        'Todos los campos son obligatorios.*',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            .copyWith(fontSize: 14, color: Colors.black45),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Número de Carnet De Identidad',
+                            style: headlineTextsTheme,
+                          ),
+                          GestionUhDefaultTextField(
+                            hintText: '###########',
+                            autovalidateMode: AutovalidateMode.disabled,
+                            controller: ciController,
+                            validator: identityNumberCIValidator,
+                            keyboardType: TextInputType.number,
+                          ),
+                        ]),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Contraseña',
+                            style: headlineTextsTheme,
+                          ),
+                          GestionUhDefaultTextField(
+                            hintText: '********',
+                            autovalidateMode: AutovalidateMode.disabled,
+                            controller: passwordController,
+                            validator: safetyPasswordValidator,
+                            keyboardType: TextInputType.visiblePassword,
+                            borderRadius: BorderRadius.only(
+                              topLeft: const Radius.circular(5),
+                              bottomLeft: const Radius.circular(5),
+                            ),
+                          ),
+                        ]),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Introduzca respuesta para las preguntas de seguridad de su preferencia',
                       style: Theme.of(context)
                           .textTheme
                           .headline6
                           .copyWith(fontSize: 14, color: Colors.black45),
                       textAlign: TextAlign.center,
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Número de Carnet De Identidad',
-                          style: headlineTextsTheme,
-                        ),
-                        GestionUhDefaultTextField(
-                          controller: ciController,
-                          keyboardType: TextInputType.number,
-                          hintText: '###########',
-                          validator: identityNumberCIValidator,
-                        ),
-                      ]),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Contraseña',
-                          style: headlineTextsTheme,
-                        ),
-                        GestionUhDefaultTextField(
-                          hintText: '********',
-                          controller: passwordController,
-                          validator: safetyPasswordValidator,
-                          keyboardType: TextInputType.visiblePassword,
-                          borderRadius: BorderRadius.only(
-                            topLeft: const Radius.circular(5),
-                            bottomLeft: const Radius.circular(5),
-                          ),
-                        ),
-                      ]),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Introduzca respuesta para las preguntas de seguridad de su preferencia',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6
-                        .copyWith(fontSize: 14, color: Colors.black45),
-                    textAlign: TextAlign.center,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: () {
-                      var childrenQuest = <Widget>[];
-                      for (int i = 0;
-                          i < NUMBER_OF_SECURITY_QUESTIONS_NEEDED;
-                          i++) {
-                        childrenQuest.add(buildQuestionZone(i));
-                      }
-                      return childrenQuest;
-                    }(),
-                  ),
-                  SizedBox(height: 10),
-                  GestionUhDefaultButton(
-                    text: 'Registrar',
-                    onPressed: () => null,
-                  ),
-                  SizedBox(height: 30),
-                ],
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: () {
+                        var childrenQuest = <Widget>[];
+                        for (int i = 0;
+                            i < NUMBER_OF_SECURITY_QUESTIONS_NEEDED;
+                            i++) {
+                          childrenQuest.add(buildQuestionZone(i));
+                        }
+                        return childrenQuest;
+                      }(),
+                    ),
+                    SizedBox(height: 10),
+                    GestionUhDefaultButton(
+                      text: 'Registrar',
+                      onPressed: _onRegisterAction,
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
               ),
             ),
           );
@@ -237,6 +279,7 @@ class _RegisterPageState extends State<RegisterPage> {
           questionsTaken.indexOf(index) != -1
               ? GestionUhDefaultTextField(
                   hintText: 'Respuesta No.${index + 1}',
+                  validator: answerValidator,
                   controller: answersTextControllers[index],
                 )
               : Container(),
@@ -246,7 +289,7 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   void _showCenterFlash({
-    String error,
+    String message,
     FlashPosition position,
     FlashStyle style,
     Alignment alignment,
@@ -270,7 +313,7 @@ class _RegisterPageState extends State<RegisterPage> {
             child: DefaultTextStyle(
               style: TextStyle(color: Colors.white),
               child: Text(
-                error,
+                message,
               ),
             ),
           ),
