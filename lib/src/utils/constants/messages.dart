82248a0284258c0b15f0fea7147c955efce39fb8
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-04-17 19:21:07 -0400

    Add constants for terms and conditions messages

---
+++ lib/src/utils/constants/messages.dart
@@ -0,0 +1,31 @@
+// TERMS AND CONDITIONS TEXTS
+const TERMS_AND_CONDITIONS_OF_USE_TITLE =
+    'ÉTICA PARA EL EMPLEO DE LAS TECNOLOGÍAS EN LA UNIVERSIDAD DE LA HABANA';
+
+const TERMS_AND_CONDITIONS_OF_USE_SUBTITLE =
+    ' Las redes y servicios de la Universidad de la Habana tienen como objetivo facilitar el intercambio de información, aprendizaje y trabajo colaborativo de sus usuarios, los que se comprometen a cumplir los siguientes principios en el uso de las tecnologías: ';
+
+const TERMS_AND_CONDITIONS_OF_USE_ENUM_TERMS =
+    '\n1.Cumplir con las disposiciones de seguridad informática vigentes a nivel nacional. '
+    '\n'
+    '\n2.No realizar hechos delictivos ni lesivos a la dignidad humana. '
+    '\n'
+    '\n3.No perjudicar a personas o instituciones. '
+    '\n'
+    '\n4.No acceder sin autorización a información ajena. '
+    '\n'
+    '\n5.No acceder o difundir información falsa, ni contraria a los principios éticos y morales de la sociedad. '
+    '\n'
+    '\n6.No enviar spam. '
+    '\n'
+    '\n7.No apropiarse de la producción intelectual de otras personas. '
+    '\n'
+    '\n8.Ser responsable en el uso, diseño y desarrollo de aplicaciones informáticas. '
+    '\n'
+    '\n9.Proteger los medios e información digital bajo su responsabilidad. '
+    '\n'
+    '\n10.Alertar a la administración sobre comportamientos anormales en la red o los servicios, que puedan ser producto de ataques informáticos u otras causas. '
+    '\n'
+    '\nLas cuentas asignadas a los usuarios serán auditadas con vistas a verificar el cumplimiento de la ética.  '
+    '\n'
+    '\nAcepto cumplir con todo lo antes expuesto.';
