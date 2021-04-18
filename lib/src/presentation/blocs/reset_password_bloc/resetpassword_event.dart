da3afa2fd9095e64357fc85d895ae0b7da59f644
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-04-17 21:56:03 -0400

    Add new entry for the current password in reset password page

--- lib/src/presentation/blocs/reset_password_bloc/resetpassword_event.dart
+++ lib/src/presentation/blocs/reset_password_bloc/resetpassword_event.dart
@@ -5,10 +5,12 @@ abstract class ResetPasswordEvent {
 }
 
 class ResetPasswordAttempted extends ResetPasswordEvent {
+  final String currentPassword;
   final String passwordFirst;
   final String passwordSecond;
 
   const ResetPasswordAttempted({
+    required this.currentPassword,
     required this.passwordFirst,
     required this.passwordSecond,
   });
